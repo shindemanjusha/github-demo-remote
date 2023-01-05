4607d4591caeec6706649b3e142871e2dd023e5b
shindemanjusha <122008735+shindemanjusha@users.noreply.github.com>
2023-01-05 15:01:05 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#add implementation
+def add(x,y):
+    return x+y;
+
+#subtract implementation
+def subtract(x,y):
+    return x-y #on master branch
+
+#multiply implementation
+def multiply(x,y):
+    return x*y #In bug456 branch
+
+#divide implementation
+def divide(x,y):
+    if y==0     #On master branch
+        return DIVIDE_BY_0_ERROR
+    else
+        return x/y
+
+#square implementation        
+def square(x);
+    return x*x
