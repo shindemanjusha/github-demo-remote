f730dfb4c5a21f8b9998a7b846cfa39adedade3e
manjusha_shinde <shinde.manjusha@gmail.com>
2023-01-05 16:03:38 +0530

    Implemented multiply function on local repo

--- math.py
+++ math.py
@@ -7,7 +7,7 @@ def subtract(x,y):
     return x-y #On remote main repository
 #multiply implementation
 def multiply(x,y):
-    pass
+    return x*y # On local repo
 #divide implementation
 def divide(x,y):
     pass
